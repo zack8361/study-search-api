4f5aa5a5b94b7e99e770b82e0b035ea341beb5ab
이찬호 <zack6353@cowave.kr>
2024-10-28 22:18:32 +0900

    first commit

---
+++ src/main/java/com/study/demo/search_api/SearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.demo.search_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/demo/search_api/SearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.demo.search_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
